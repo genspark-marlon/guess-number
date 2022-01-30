89b9a966606d07d06dcb2c4af20a1ef7e6ebed1f
marlonaesparza <marlonaesparza@gmail.com>
2022-01-29 17:29:55 -0800

    Refactored Code:
    -Main -> Game.
    -Game.main -> Inits Game.
    -Game.initGame -> Begins game.
    -Game.(utilmethods) => Help with game logic, and keeps code dry.

--- src/main.java
+++ src/main.java
@@ -1,61 +1,81 @@
 import java.util.Scanner;
 
-public class Main {
-  public static void main (String[] args) {
-    int randomNum = (int) (Math.random() * 20 + 1);
-    int totalGuesses = 0;
-    int maxGuesses = 6;
-    int userGuess;
-    String userName;
+public class Game {
+  // game number vars
+  private int randomNum = (int) (Math.random() * 20 + 1);
+  private boolean newNum;
+
+  // user guess vars
+  private int maxGuesses = 6;
+  private int totalGuesses;
+
+  // user input vars
+  private int userGuess;
+  private String userName;
+  private String userDecision;
+
+  // user guess method
+  public void tooHighOrLow () {
+    if (this.userGuess < this.randomNum) {
+      System.out.println("Your guess is too low.");
+    } else if (this.userGuess > this.randomNum) {
+      System.out.println("Your guess is too high.");
+    }
+    this.totalGuesses++;
+  }
+
+  // user decision method
+  public void endOrContinue () {
+    if (this.userDecision.toLowerCase().equals("y")) {
+      this.randomNum = (int) (Math.random() * 20 + 1);
+      this.totalGuesses = 0;
+      this.newNum = true;
+    } else {
+      this.userDecision = "n";
+      System.out.println("Thanks for playing.");
+    }
+  }
+
+  public static void gameOver () {
+    System.out.println("Game is over.");
+  }
+
+  // initialize game method
+  public void initGame () {
     Scanner userInput = new Scanner(System.in);
-
     System.out.println("Hello! What is your name?");
-
-    userName = userInput.nextLine();
-
+    this.userName = userInput.nextLine();
     System.out.println(String.format("Well, %s, I am thinking of a number between 1 and 20.", userName));
 
-    do {
-      System.out.println("Take a guess.");
+    try {
+      do {
+        System.out.println("Take a guess.");
+        this.userGuess = userInput.nextInt();
+        userInput.nextLine();
 
-      userGuess = userInput.nextInt();
-      userInput.nextLine();
-
-      if (userGuess < randomNum) {
-	System.out.println("Your guess is too low.");
-        totalGuesses++;
-
-      } else if (userGuess > randomNum) {
-        System.out.println("Your guess is too high.");
-        totalGuesses++;
-
-      } else if (userGuess == randomNum) {
-        System.out.println(String.format("Good job, %s! You guessed my number in %s guesses!\nWould you like to play again? (y/n)", userName, totalGuesses + 1));
-        String userDecision1 = userInput.nextLine();
-
-        if (userDecision1.toLowerCase().equals("y")) {
-          randomNum = (int) (Math.random() * 20 + 1);
-          totalGuesses = 0;
-        } else {
-          System.out.println("Thanks for playing. See you next time!");
-          totalGuesses = 6;
-          userInput.close();
+        if (this.userGuess < this.randomNum || this.userGuess > this.randomNum) {
+          this.tooHighOrLow();
+        } else if (this.userGuess == this.randomNum) {
+          System.out.println(String.format("Good job, %s! You guessed my number in %s guesses!\nWould you like to play again? (y/n)", userName, totalGuesses + 1));
+          this.userDecision = userInput.nextLine();
+          this.endOrContinue();
+        } else if (this.totalGuesses == 6) {
+          System.out.println(String.format("Game over! You've failed %s times.\nPlay again? (y/n)", totalGuesses));
+          this.userDecision = userInput.nextLine();
+          this.endOrContinue();
         }
+      } while (this.totalGuesses < this.maxGuesses && this.userDecision != "n");
 
-      }
+    } catch (Exception e) {
+      System.out.println("Error:" + e);
+    } finally {
+      userInput.close();
+      this.gameOver();
+    }
+  }
 
-      if (userGuess != randomNum && totalGuesses == 6) {
-        System.out.println(String.format("Game over! You've failed %s times.\nPlay again? (y/n)", totalGuesses));
-        String userDecision2 = userInput.nextLine();
-
-        if (userDecision2.toLowerCase().equals("y")) {
-          randomNum = (int) (Math.random() * 20 + 1);
-          totalGuesses = 0;
-        } else {
-          System.out.println("Thanks for playing.");
-          userInput.close();
-        }
-      }
-    } while (totalGuesses < maxGuesses);
+  public static void main (String[] args) {
+    Game guessNumberGame = new Game();
+    guessNumberGame.initGame();
   }
 }
